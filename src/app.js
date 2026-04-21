ea1d4d28d5a8a7c66107a9f5a744c8af703e4701
nkl0zip <156062676+nkl0zip@users.noreply.github.com>
2026-04-21 16:40:02 +0530

    updated cors policies

--- src/app.js
+++ src/app.js
@@ -32,11 +32,20 @@ const app = express();
 
 app.use(
   cors({
-    origin: (origin, callback) => callback(null, true),
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin); // 🔥 return exact origin
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
 app.use(helmet());
 app.use(morgan("dev"));
