e741dca13e8cee19415c2a79aa6384cde802983b
nkl0zip <156062676+nkl0zip@users.noreply.github.com>
2026-04-21 16:50:45 +0530

    cross origin updated

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -30,7 +30,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie("adminToken", token, {
       httpOnly: true,
-      secure: true,
+      secure: false,
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
